db27f66dfcf941aa8382487b429709fcbfa9f2a7
moaskary20 <moaskary20@gmail.com>
2026-03-25 08:26:23 +0200

    video player fix4

--- resources/views/components/site/youtube-embed.blade.php
+++ resources/views/components/site/youtube-embed.blade.php
@@ -1,6 +1,7 @@
 {{--
-    تضمين YouTube مع طبقة تغطية على الزاوية العلوية اليمنى لحجب Share و Watch later
-    (لا يوجد معامل رسمي في واجهة اليوتيوب لإخفائهما).
+    تضمين YouTube مع شريط تحكم افتراضي (تشغيل / صوت / تقدّم).
+    شريط علوي يحجب عنوان الفيديو وأيقونة القناة و Share / Watch later (لا يوجد معامل رسمي لإخفائها).
+    شعار YouTube في الزاوية السفلية يُقلّل عبر modestbranding في رابط الـ embed.
 --}}
 @props([
     'src',
@@ -27,8 +28,9 @@
         allowfullscreen
         referrerpolicy="strict-origin-when-cross-origin"
     ></iframe>
+    {{-- لا يغطي شريط التحكم السفلي داخل الـ iframe --}}
     <div
-        class="pointer-events-auto absolute top-0 right-0 z-[1] h-[5rem] w-[min(55%,260px)] bg-gradient-to-bl from-black from-35% via-black/90 to-transparent sm:h-[5.5rem] sm:w-[min(50%,280px)]"
+        class="pointer-events-auto absolute inset-x-0 top-0 z-[1] h-[5.75rem] bg-gradient-to-b from-black from-30% via-black/80 to-transparent sm:h-28"
         aria-hidden="true"
     ></div>
 </div>
