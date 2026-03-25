6f6c8c884b4b8bec55c75a83ad42579bac351609
moaskary20 <moaskary20@gmail.com>
2026-03-25 20:46:07 +0200

    user roll2

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -10,19 +10,8 @@ class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
 
-    /** @var list<int|string> */
-    protected array $rolesToSync = [];
-
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        $raw = $data['roles'] ?? null;
-        if (is_array($raw)) {
-            $this->rolesToSync = array_values(array_filter($raw, fn ($id) => $id !== null && $id !== ''));
-        } elseif ($raw !== null && $raw !== '') {
-            $this->rolesToSync = [$raw];
-        } else {
-            $this->rolesToSync = [];
-        }
         unset($data['roles']);
 
         $data['phone'] = ! empty($data['phone'] ?? null)
@@ -30,14 +19,5 @@ class CreateUser extends CreateRecord
             : null;
 
         return $data;
-    }
-
-    protected function afterCreate(): void
-    {
-        if ($this->rolesToSync !== []) {
-            $this->record->syncRoles($this->rolesToSync);
-            $this->record->unsetRelation('roles');
-        }
-        $this->rolesToSync = [];
     }
 }
--- app/Filament/Resources/Users/Pages/EditUser.php
+++ app/Filament/Resources/Users/Pages/EditUser.php
@@ -11,9 +11,6 @@ class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
-    /** @var list<int|string> */
-    protected array $rolesToSync = [];
-
     protected function getHeaderActions(): array
     {
         return [
@@ -23,14 +20,7 @@ class EditUser extends EditRecord
 
     protected function mutateFormDataBeforeSave(array $data): array
     {
-        $raw = $data['roles'] ?? null;
-        if (is_array($raw)) {
-            $this->rolesToSync = array_values(array_filter($raw, fn ($id) => $id !== null && $id !== ''));
-        } elseif ($raw !== null && $raw !== '') {
-            $this->rolesToSync = [$raw];
-        } else {
-            $this->rolesToSync = [];
-        }
+        // Multi relationship Select is not dehydrated; roles sync inside Form::getState() via saveRelationships().
         unset($data['roles']);
 
         $data['phone'] = ! empty($data['phone'] ?? null)
@@ -38,13 +28,6 @@ class EditUser extends EditRecord
             : null;
 
         return $data;
-    }
-
-    protected function afterSave(): void
-    {
-        $this->record->syncRoles($this->rolesToSync);
-        $this->record->unsetRelation('roles');
-        $this->rolesToSync = [];
     }
 
     protected function mutateFormDataBeforeFill(array $data): array
